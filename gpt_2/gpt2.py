f1e9731ef0ac0c7ebb4f4e3aa924cff4c23916c6
Hugo Albert <hugoalberthlu@gmail.com>
2024-06-25 15:14:24 +0200

    tensorfloat32

--- gpt_2/gpt2.py
+++ gpt_2/gpt2.py
@@ -3,6 +3,8 @@ import math
 from dataclasses import dataclass
 import torch.nn as nn
 from torch.nn import functional as F
+import tiktoken
+import time
 
 class CausalSelfAttention(nn.Module):
     def __init__(self, config):
@@ -11,6 +13,7 @@ class CausalSelfAttention(nn.Module):
 
         self.c_attn = nn.Linear(config.n_embd, 3* config.n_embd)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         self.n_head = config.n_head
         self.n_embd = config.n_embd
 
@@ -22,11 +25,11 @@ class CausalSelfAttention(nn.Module):
 
         qkv = self.c_attn(x)
         q, k, v = qkv.split(self.n_embd, dim = 2)
-        k = k.view(B, T, C//self.n_head).transpose(1,2)
-        q = q.view(B, T, C//self.n_head).transpose(1,2)
-        v = v.view(B, T, C//self.n_head).transpose(1,2)
+        k = k.view(B, T, self.n_head,C//self.n_head).transpose(1,2)
+        q = q.view(B, T, self.n_head, C//self.n_head).transpose(1,2)
+        v = v.view(B, T, self.n_head, C//self.n_head).transpose(1,2)
 
-        att = (q @ k.transpose(-2, -1) * (1.0/math.sqrt(k.size())))
+        att = (q @ k.transpose(-2, -1) * (1.0/math.sqrt(k.size(-1))))
         att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float("-inf"))
         att = F.softmax(att, dim = -1)
         y = att @ v
@@ -42,12 +45,15 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate="tanh")
         self.c_proj = nn.Linear(4*config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
         x = self.gelu(x)
         x = self.c_proj(x)
         return x
+    
+
 class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -83,6 +89,23 @@ class GPT(nn.Module):
         ))
 
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias = False)
+
+        #weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
+        #init params
+        self.apply(self._init_weights) #iterates each module and applies the function
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, "NANOGPT_SCALE_INIT"):
+                std += (2 * self.config.n_layer) ** -0.5 # each module has two blocks
+            torch.nn.init.normal_(module.weight, mean = 0.0, std = 0.02) #usually would like something that grows with the model
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean = 0.0, std = 0.02)
 
     @classmethod
     def from_pretrained(cls, model_type):
@@ -133,10 +156,153 @@ class GPT(nn.Module):
 
         return model
     
-    #def forward():
+    def forward(self, idx, targets = None):
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block"
+
+        pos = torch.arange(0, T, dtype = torch.long, device = idx.device)
+        pos_emb = self.transformer.wpe(pos)
+        tok_emb = self.transformer.wte(idx)
+
+        x = tok_emb + pos_emb
+
+        for block in self.transformer.h:
+            x = block(x)
+
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x) #(B, T, vocab_size)
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
+
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        with open("original_transformer/input.txt", "r") as f:
+            text = f.read()
+
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens)//(B*T)} batches")
+
+        self.current_position = 0
+
+    def next_batch(self):
+        B,T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position + B*T + 1]
+        x = (buf[:-1]).view(B, T)
+        y = (buf[1:]).view(B, T)
+
+        self.current_position += B*T
+
+        if self.current_position + (B*T + 1) > len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+
+def print_gpt2_results():
+    num_return_sequences = 5
+    max_length = 30
+
+    model = GPT.from_pretrained("gpt2")
+    model.eval() #to evaluation mode for layers like dropout
+    model.to("cuda")
+
+    
+    enc = tiktoken.get_encoding("gpt2")
+    tokens = enc.encode("Hello, I'm a language model,")
+    tokens = torch.tensor(tokens, dtype=torch.long)
+    tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+    x = tokens.to("cuda")
+
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+    while x.size(1) < max_length:
+        with torch.no_grad():
+            logits = model(x)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim = -1)
+            topk_probs, topk_indices = torch.topk(probs, 50, dim = -1)
+            ix = torch.multinomial(topk_probs, 1)
+            xcol = torch.gather(topk_indices, -1, ix)
+            x = torch.cat((x, xcol), dim = 1)
+
+    for i in range(num_return_sequences):
+        tokens = x[i, :max_length].tolist()
+        decoded = enc.decode(tokens)
+        print(">", decoded)
+
+
 
 
 ##########################
 if __name__ == "__main__":
-    model = GPT.from_pretrained("gpt2")
-    print("it worked!")
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+        device = "mps"
+    print(f'using device: {device}')
+
+    torch.manual_seed(1337)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(1337)
+
+    train_loader = DataLoaderLite(B = 4, T = 1024)
+
+    torch.set_float32_matmul_precision("high") #TensorFloat32, more efficient but less precision
+
+    model = GPT(GPTConfig())
+    model.eval() #to evaluation mode for layers like dropout
+    model.to(device)
+    # model = torch.compile(model) #compiles the code, needs compiling time but then it is faster
+    # torch.compile is just for linux
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+    for i in range(50):
+        t0 = time.time()
+        x, y = train_loader.next_batch()
+        x, y = x.to(device), y.to(device)
+        optimizer.zero_grad() #always start with zero gradients
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):  #for mixed precision, more efficiency
+            logits, loss = model(x, y)
+        loss.backward() #always adds to the gradients, thats why they need to be zero
+        optimizer.step()
+        torch.cuda.synchronize() #wait for the GPU to finish all the scheduled work
+        t1 = time.time()
+        dt = (t1 - t0)*1000
+        tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+        print(f"step {i}, loss: {loss.item()}, dt: {dt} ms, tok/sec: {tokens_per_sec}")
+
+
+
+    import sys;sys.exit(0)
+
+    
+    tokens = enc.encode("Hello, I'm a language model,")
+    tokens = torch.tensor(tokens, dtype=torch.long)
+    tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+    x = tokens.to(device)
+
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+    while x.size(1) < max_length:
+        with torch.no_grad():
+            logits = model(x)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim = -1)
+            topk_probs, topk_indices = torch.topk(probs, 50, dim = -1)
+            ix = torch.multinomial(topk_probs, 1)
+            xcol = torch.gather(topk_indices, -1, ix)
+            x = torch.cat((x, xcol), dim = 1)
+
+    for i in range(num_return_sequences):
+        tokens = x[i, :max_length].tolist()
+        decoded = enc.decode(tokens)
+        print(">", decoded)
